102e8f4b6f08a4d9547a116c2cfd48b2ee23a013
Yang Chen <jackchen.chen@mail.utoronto.ca>
2022-06-17 22:16:48 -0400

    realize PCF

--- Lab1/src/shaders/phongShader/phongFragment.glsl
+++ Lab1/src/shaders/phongShader/phongFragment.glsl
@@ -31,84 +31,104 @@ varying vec4 vPositionFromLight;
 #define BIAS_COEF 0.007
 #define BIAS BIAS_COEF * max( 0.0 , 1.0 - dot(normalize(uLightPos), normalize(vNormal)) )
 
-highp float rand_1to1(highp float x ) { 
+highp float rand_1to1(highp float x) { 
   // -1 -1
-  return fract(sin(x)*10000.0);
+  return fract(sin(x) * 10000.0);
 }
 
-highp float rand_2to1(vec2 uv ) { 
+highp float rand_2to1(vec2 uv) { 
   // 0 - 1
-	const highp float a = 12.9898, b = 78.233, c = 43758.5453;
-	highp float dt = dot( uv.xy, vec2( a,b ) ), sn = mod( dt, PI );
-	return fract(sin(sn) * c);
+  const highp float a = 12.9898, b = 78.233, c = 43758.5453;
+  highp float dt = dot(uv.xy, vec2(a, b)), sn = mod(dt, PI);
+  return fract(sin(sn) * c);
 }
 
 float unpack(vec4 rgbaDepth) {
-    const vec4 bitShift = vec4(1.0, 1.0/256.0, 1.0/(256.0*256.0), 1.0/(256.0*256.0*256.0));
-    return dot(rgbaDepth, bitShift);
+  const vec4 bitShift = vec4(1.0, 1.0 / 256.0, 1.0 / (256.0 * 256.0), 1.0 / (256.0 * 256.0 * 256.0));
+  return dot(rgbaDepth, bitShift);
 }
 
 vec2 poissonDisk[NUM_SAMPLES];
 
-void poissonDiskSamples( const in vec2 randomSeed ) {
+void poissonDiskSamples(const in vec2 randomSeed) {
 
-  float ANGLE_STEP = PI2 * float( NUM_RINGS ) / float( NUM_SAMPLES );
-  float INV_NUM_SAMPLES = 1.0 / float( NUM_SAMPLES );
+  float ANGLE_STEP = PI2 * float(NUM_RINGS) / float(NUM_SAMPLES);
+  float INV_NUM_SAMPLES = 1.0 / float(NUM_SAMPLES);
 
-  float angle = rand_2to1( randomSeed ) * PI2;
+  float angle = rand_2to1(randomSeed) * PI2;
   float radius = INV_NUM_SAMPLES;
   float radiusStep = radius;
 
-  for( int i = 0; i < NUM_SAMPLES; i ++ ) {
-    poissonDisk[i] = vec2( cos( angle ), sin( angle ) ) * pow( radius, 0.75 );
+  for(int i = 0; i < NUM_SAMPLES; i++) {
+    poissonDisk[i] = vec2(cos(angle), sin(angle)) * pow(radius, 0.75);
     radius += radiusStep;
     angle += ANGLE_STEP;
   }
 }
 
-void uniformDiskSamples( const in vec2 randomSeed ) {
+void uniformDiskSamples(const in vec2 randomSeed) {
 
   float randNum = rand_2to1(randomSeed);
-  float sampleX = rand_1to1( randNum ) ;
-  float sampleY = rand_1to1( sampleX ) ;
+  float sampleX = rand_1to1(randNum);
+  float sampleY = rand_1to1(sampleX);
 
   float angle = sampleX * PI2;
   float radius = sqrt(sampleY);
 
-  for( int i = 0; i < NUM_SAMPLES; i ++ ) {
-    poissonDisk[i] = vec2( radius * cos(angle) , radius * sin(angle)  );
+  for(int i = 0; i < NUM_SAMPLES; i++) {
+    poissonDisk[i] = vec2(radius * cos(angle), radius * sin(angle));
 
-    sampleX = rand_1to1( sampleY ) ;
-    sampleY = rand_1to1( sampleX ) ;
+    sampleX = rand_1to1(sampleY);
+    sampleY = rand_1to1(sampleX);
 
     angle = sampleX * PI2;
     radius = sqrt(sampleY);
   }
 }
 
-float findBlocker( sampler2D shadowMap,  vec2 uv, float zReceiver ) {
-	return 1.0;
+float findBlocker(sampler2D shadowMap, vec2 uv, float zReceiver) {
+  return 1.0;
 }
 
 float PCF(sampler2D shadowMap, vec4 coords) {
-  return 1.0;
+
+  uniformDiskSamples(coords.xy);
+  //poissonDiskSamples(coords.xy);
+
+
+  // 将在PCSS中详细计算
+  float radiusRatio = 0.002;
+  float blockerDepth = coords.z;
+
+  int blockCount = 0;
+
+  for(int i = 0; i < NUM_SAMPLES; i++) {
+
+    vec2 samplerCoord = poissonDisk[i] * radiusRatio + coords.xy;
+    float lightDepth = unpack(texture2D(shadowMap, samplerCoord));
+
+    // 如果实际深度大于光照深度，则看不见，返回0
+    blockCount += (blockerDepth - BIAS > lightDepth) ? 0 : 1;
+  }
+ 
+  return float(blockCount) / float(NUM_SAMPLES);
 }
 
-float PCSS(sampler2D shadowMap, vec4 coords){
+float PCSS(sampler2D shadowMap, vec4 coords) {
 
   // STEP 1: avgblocker depth
-
+  
   // STEP 2: penumbra size
 
   // STEP 3: filtering
-  
+
   return 1.0;
 
 }
 
-float useShadowMap(sampler2D shadowMap, vec4 shadowCoord){
+float useShadowMap(sampler2D shadowMap, vec4 shadowCoord) {
   float lightDepth = unpack(texture2D(shadowMap, shadowCoord.xy));
-  
+
   // 如果实际深度大于光照深度，则看不见，返回0
   return shadowCoord.z - BIAS > lightDepth ? 0.0 : 1.0;
 }
@@ -122,8 +142,7 @@ vec3 blinnPhong() {
   vec3 lightDir = normalize(uLightPos);
   vec3 normal = normalize(vNormal);
   float diff = max(dot(lightDir, normal), 0.0);
-  vec3 light_atten_coff =
-      uLightIntensity / pow(length(uLightPos - vFragPos), 2.0);
+  vec3 light_atten_coff = uLightIntensity / pow(length(uLightPos - vFragPos), 2.0);
   vec3 diffuse = diff * light_atten_coff * color;
 
   vec3 viewDir = normalize(uCameraPos - vFragPos);
@@ -141,8 +160,8 @@ void main(void) {
   float visibility;
   vec3 shadowCoord = vPositionFromLight.xyz * 0.5 + 0.5;
 
-  visibility = useShadowMap(uShadowMap, vec4(shadowCoord, 1.0));
-  //visibility = PCF(uShadowMap, vec4(shadowCoord, 1.0));
+  //visibility = useShadowMap(uShadowMap, vec4(shadowCoord, 1.0));
+  visibility = PCF(uShadowMap, vec4(shadowCoord, 1.0));
   //visibility = PCSS(uShadowMap, vec4(shadowCoord, 1.0));
 
   vec3 phongColor = blinnPhong();
