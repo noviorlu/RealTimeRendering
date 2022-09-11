8f7eab665710630a338becd12fe8f3c15281a18e
Yang Chen <jackchen.chen@mail.utoronto.ca>
2022-09-11 16:51:12 -0400

    reinstall lab2

--- Lab2/prt/build/ext_build/openexr/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ychen/projects/GAMES202/Lab2/prt/ext/openexr/zlib
-# Build directory: C:/Users/ychen/projects/GAMES202/Lab2/prt/build/ext_build/openexr/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
