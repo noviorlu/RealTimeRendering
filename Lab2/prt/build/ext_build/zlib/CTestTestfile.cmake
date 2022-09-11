4fc63520e03db6b3b0504fb5e84ccf6dcac038fc
Yang Chen <jackchen.chen@mail.utoronto.ca>
2022-09-11 16:43:25 -0400

    move gitignore

---
+++ Lab2/prt/build/ext_build/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/ychen/projects/GAMES202/Lab2/prt/ext/zlib
+# Build directory: C:/Users/ychen/projects/GAMES202/Lab2/prt/build/ext_build/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
